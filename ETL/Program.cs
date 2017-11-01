ee4d7462a09d05b26c67e0ffd0d4991e24563cc2
Marcos Moreno <marcosmorenomamg@gmail.com>
2017-11-01 13:59:14 -0600

    First commit, adding FTP Client class

---
+++ ETL/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Configuration;
+namespace ETL
+{
+    public class FTPClient
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string URL { get; set; }
+        public int Port { get; set; }
+
+        public FTPClient(string _userName, string _password, string _URL, int _port)
+        {
+            UserName = _userName;
+            Password = _password;
+            URL = _URL;
+            Port = _port;
+        }
+
+        public void UploadFile()
+        {
+            // Get the object used to communicate with the server.  
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(UserName);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // This example assumes the FTP site uses anonymous logon.  
+            request.Credentials = new NetworkCredential("anonymous", URL);
+
+            // Copy the contents of the file to the request stream.  
+            StreamReader sourceStream = new StreamReader("testfile.txt");
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+
+            response.Close();
+        }
+    }
+}
