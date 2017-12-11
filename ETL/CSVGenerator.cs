cb2f9a7787c916c53f76328452cf8b2aa7cbc7f2
Marcos Moreno <marcosmorenomamg@gmail.com>
2017-12-11 16:58:12 -0600

    Separating SqlOperations class

--- ETL/CSVGenerator.cs
+++ ETL/CSVGenerator.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System;
 using System.Data;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace ETL
 {    
@@ -48,8 +47,8 @@ namespace ETL
                     {
                         if (!excludedColumns.Contains(columns[i].ColumnName))
                         {
-                            //TODO: REPLACE COMMAS
-                            csv.WriteField(row[i]);
+                            string field = row[i].ToString().Replace(",", "");
+                            csv.WriteField(field);
                         }
                             
                     }
@@ -66,67 +65,5 @@ namespace ETL
             }
             return memoryStream;
         }
-    }
-    class SqlOperations
-    {
-        public string Operation { get; set; }
-        public List<string> Params { get; set; }
-        public string ColumnToReplace { get; set; }
-        public string DefaultValue { get; set; }
-
-        public List<SqlOperations> ExtractOperations(DataRow row)
-        {
-            List<SqlOperations> sqlOperations = new List<SqlOperations>();
-            try
-            {
-                sqlOperations = JsonConvert.DeserializeObject<List<SqlOperations>>(row["Expressions"].ToString());
-            }
-            catch (Exception ex){}
-            return sqlOperations;
-        }
-
-        public DataRow[] PerformOperations(SqlOperations sqlOperations, DataRow[] dataRows)
-        {
-            switch (sqlOperations.Operation)
-            {
-                case "isnull":
-                    foreach (DataRow row in dataRows)
-                    {
-                        try
-                        {
-                            row[sqlOperations.ColumnToReplace] = row[sqlOperations.Params[0]] ?? row[sqlOperations.Params[1]];
-                        }
-                        catch
-                        {
-                            row[sqlOperations.ColumnToReplace] = sqlOperations.DefaultValue;
-                        }                       
-                    }
-                    break;
-                case "between":
-                    foreach (DataRow row in dataRows)
-                    {
-                        try
-                        {
-                            if (DateTime.TryParse(row[sqlOperations.Params[0]].ToString(), out DateTime startingDate)
-                                && DateTime.TryParse(row[sqlOperations.Params[1]].ToString(), out DateTime EndingDate))
-                            {                                
-                                row[sqlOperations.ColumnToReplace] = DateTime.Today >= startingDate
-                                                                    && DateTime.Today <= EndingDate ?
-                                                                    row[sqlOperations.Params[2]] : sqlOperations.DefaultValue;
-                            }
-                            else
-                            {
-                                row[sqlOperations.ColumnToReplace] = sqlOperations.DefaultValue;
-                            }
-                        }
-                        catch
-                        {
-                            row[sqlOperations.ColumnToReplace] = sqlOperations.DefaultValue;
-                        }                        
-                    }
-                    break;
-            };
-            return dataRows;
-        }
-    }
+    }    
 }
---
+++ ETL/SqlOperations.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ETL
+{
+    class SqlOperations
+    {
+        public string Operation { get; set; }
+        public List<string> Params { get; set; }
+        public string ColumnToReplace { get; set; }
+        public string DefaultValue { get; set; }
+
+        public List<SqlOperations> ExtractOperations(DataRow row)
+        {
+            List<SqlOperations> sqlOperations = new List<SqlOperations>();
+            try
+            {
+                sqlOperations = JsonConvert.DeserializeObject<List<SqlOperations>>(row["Expressions"].ToString());
+            }
+            catch (Exception ex) { }
+            return sqlOperations;
+        }
+
+        public DataRow[] PerformOperations(SqlOperations sqlOperations, DataRow[] dataRows)
+        {
+            switch (sqlOperations.Operation)
+            {
+                case "isnull":
+                    foreach (DataRow row in dataRows)
+                    {
+                        try
+                        {
+                            row[sqlOperations.ColumnToReplace] = row[sqlOperations.Params[0]] ?? row[sqlOperations.Params[1]];
+                        }
+                        catch
+                        {
+                            row[sqlOperations.ColumnToReplace] = sqlOperations.DefaultValue;
+                        }
+                    }
+                    break;
+                case "between":
+                    foreach (DataRow row in dataRows)
+                    {
+                        try
+                        {
+                            if (DateTime.TryParse(row[sqlOperations.Params[0]].ToString(), out DateTime startingDate)
+                                && DateTime.TryParse(row[sqlOperations.Params[1]].ToString(), out DateTime EndingDate))
+                            {
+                                row[sqlOperations.ColumnToReplace] = DateTime.Today >= startingDate
+                                                                    && DateTime.Today <= EndingDate ?
+                                                                    row[sqlOperations.Params[2]] : sqlOperations.DefaultValue;
+                            }
+                            else
+                            {
+                                row[sqlOperations.ColumnToReplace] = sqlOperations.DefaultValue;
+                            }
+                        }
+                        catch
+                        {
+                            row[sqlOperations.ColumnToReplace] = sqlOperations.DefaultValue;
+                        }
+                    }
+                    break;
+            };
+            return dataRows;
+        }
+    }
+}
